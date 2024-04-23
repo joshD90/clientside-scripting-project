01ac5c4f510be75b5c5af8453f795d89b3a6acbe
Joshua Dancey <94985202+joshD90@users.noreply.github.com>
2024-04-23 20:26:27 +0100

    Logo and misc (#45)
    * making a slew of optimizations across the site
    
    * updating logo, adding bannerfont, optimising products lazy loading, moving scripts on landing page

--- products/index.js
+++ products/index.js
@@ -3,7 +3,7 @@ import { setModalDetails } from "./modal.js";
 
 const productContainer = document.getElementById("product-container");
 
-const createCard = (carDetails) => {
+const createCard = (carDetails, index) => {
   //create our elements
   const gridCellContainer = document.createElement("div");
   const cardContainer = document.createElement("div");
@@ -42,8 +42,8 @@ const createCard = (carDetails) => {
   carDesc.classList.add("card-text");
   carPrice.classList.add("card-text");
 
-  //for optimization do lazy loading
-  cardImg.loading = "lazy";
+  //for optimization do lazy loading - however if we lazy load images that initially on screen this can actually slow the site down - so we ensure that the first 6 are not lazily loaded
+  if (index > 5) cardImg.loading = "lazy";
 
   //append to our document in reverse order
   cardBody.append(carDesc, carPrice);
